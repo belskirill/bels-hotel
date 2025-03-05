7c672d44d23de690b94f458a21c5ffb8c83b21d0
KirillBels <k.belskiy@icloud.com>
2025-03-05 23:15:54 +0300

    review

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,8 +1,17 @@
+from sqlalchemy import select
+
 from src.models.users import UsersOrm
 from src.repositories.base import BaseRepository
-from src.schemas.users import User
-
+from src.schemas.users import User, UserWithHashPassword
+from pydantic import EmailStr
 
 class UsersRepository(BaseRepository):
     model = UsersOrm
     schema = User
+
+
+    async def get_user_with_hashed_password(self, email: EmailStr):
+        query = select(self.model).filter_by(email=email)
+        results = await self.session.execute(query)
+        model = results.scalars().one()
+        return UserWithHashPassword.model_validate(model)
