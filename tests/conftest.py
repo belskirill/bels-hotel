2e8072040ecb04fba71c814895894a812df52357
KirillBels <k.belskiy@icloud.com>
2025-03-24 21:04:07 +0300

    add

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,24 @@
+from pathlib import Path
 from typing import AsyncGenerator
-
+import aiofiles
 from sqlalchemy.ext.asyncio import create_async_engine
 import pytest
-
+import json
 from httpx import AsyncClient
 from httpx import ASGITransport
 
+from src.api.dependencies import DBDep
 from src.config import settings
-from src.database import Base, engine_null_pool
+from src.database import Base, engine_null_pool, async_session_maker_null_pool
 from src.main import app
 from src.models import *
+from src.schemas.hotels import HotelAdd
+from src.schemas.rooms import RoomsAdd
+from src.utils.db_manager import DBManager
 
+current_dir = Path(__file__).parent
+file_path = current_dir / "mock_hotels.json"
+file_path2 = current_dir / "mock_rooms.json"
 
 @pytest.fixture(scope='session', autouse=True)
 async def database_setup():
@@ -18,6 +26,26 @@ async def database_setup():
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+
+    async with aiofiles.open(file_path, mode='r', encoding='utf-8') as file:
+        content = await file.read()
+        data = json.loads(content)
+        add_data_hotels = [HotelAdd(**item) for item in data]
+
+    async with aiofiles.open(file_path2, mode='r', encoding='utf-8') as file:
+        content = await file.read()
+        data = json.loads(content)
+        add_data_rooms = [RoomsAdd(**item) for item in data]
+
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        await db.hotels.add_bulk(add_data_hotels)
+        await db.rooms.add_bulk(add_data_rooms)
+
+
+        await db.commit()
+
+
 
 
 
@@ -35,6 +63,8 @@ async def async_client(database_setup):
                 'password': '1234',
             }
         )
+
+
 
 
 
