a2597d634ad18e446a6975827118bf204340c153
KirillBels <k.belskiy@icloud.com>
2025-03-27 20:40:18 +0300

    add new test_db

--- tests/integration_tests/bookings/test_db.py
+++ tests/integration_tests/bookings/test_db.py
@@ -16,3 +16,24 @@ async def test_add_booking(db):
     )
     await db.bookings.add_data(booking_data)
     await db.commit()
+
+
+    res = await db.bookings.get_one_or_none(user_id=booking_data.user_id)
+    assert res
+
+
+    new_booking_data = BookingAdd(
+        user_id=user,
+        room_id=room,
+        date_from=date(year=2024, month=8, day=20),
+        date_to=date(year=2024, month=8, day=10),
+        price=50000,
+    )
+
+    await db.bookings.edit(new_booking_data, user_id=booking_data.user_id)
+    await db.commit()
+
+
+
+    await db.bookings.delete(user_id=booking_data.user_id)
+    await db.commit()
