bc15daea6b2644120f0d217bc90a392f84007b8b
KirillBels <k.belskiy@icloud.com>
2025-04-02 15:39:03 +0300

    add new tests in module auth

---
+++ tests/integration_tests/auth/test_api.py
@@ -0,0 +1,67 @@
+import pytest
+
+
+
+@pytest.mark.parametrize('email, password, status_code', [
+    ('kirilltest@test.com', 'password', 200),
+    ('dd', 'password', 422),
+    ('@mail.ru', 'password', 422),
+])
+async def test_register_user(email, password, status_code, ac):
+    response = await ac.post(
+        'auth/register',
+        json={
+            'email': email,
+            'password': password,
+        }
+    )
+
+    assert response.status_code == status_code
+
+
+
+
+@pytest.mark.parametrize('email, password, status_code', [
+    ('kirilltest@test.com', 'password', 200),
+    ('kirilltest@test.com', 'password1234', 401),
+    ('test@test212121212.ru', 'password', 401),
+    ('test2212.ru', 'password', 422),
+])
+async def test_login_user(email, password, status_code, ac):
+    response = await ac.post(
+        'auth/login',
+        json={
+            'email': email,
+            'password': password,
+        }
+    )
+
+    assert response.status_code == status_code
+    assert ac.cookies['access_token']
+
+
+
+@pytest.mark.parametrize('email, status_code', [
+    ('kirilltest@test.com',  200),
+])
+async def test_user_me(email, status_code, ac):
+    response = await ac.get(
+        'auth/me',
+    )
+
+    _data_ = response.json()
+    assert response.status_code == status_code
+    assert _data_['email'] == email
+
+
+
+
+async def test_logout_user(ac):
+    response = await ac.post(
+        'auth/logout',
+    )
+
+    assert response.status_code == 200
+    assert 'access_token' not in ac.cookies
+
+
