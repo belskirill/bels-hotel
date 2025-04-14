c5a6b951d9d51050b51ccc76efa8d9ae8a408b71
KirillBels <k.belskiy@icloud.com>
2025-04-14 12:08:01 +0300

    121121121212121212ds

--- src/api/image.py
+++ src/api/image.py
@@ -1,15 +1,17 @@
 import shutil
 
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, UploadFile, HTTPException
 
 from src.service.image import ImageService
-from src.tasks.tasks import resize_image
+
 
 router = APIRouter(prefix="/image", tags=["image"])
 
 
 @router.post("")
 async def uplodad_image(file: UploadFile):
+    if file.content_type in ["image/png", "image/jpeg", "image/jpg"]:
+        raise HTTPException(status_code=400, detail="Недопустимый тип файла")
     await ImageService().get_image(file)
 
 
